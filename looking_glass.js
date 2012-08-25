00f29ef1d2b6c4a4e3045119491a0a96caf61e7b
Fabian Linzberger <e@lefant.net>
2012-08-25 12:30:24 +0200

    reindent, bind keys and some simplification for render_flow

--- looking_glass.js
+++ looking_glass.js
@@ -13,66 +13,66 @@ var path = sankey.link();
 
 
 (function() {
-     console.log('looking_glass initializing...');
+    console.log('looking_glass initializing...');
 
-     looking_glass = {
-         version: "0.0.1"
-     };
+    looking_glass = {
+        version: "0.0.1"
+    };
 
-     var index = {};
+    var index = {};
 
-     var tbody;
+    var tbody;
 
-     looking_glass.init = function(chart0) {
-         var table = chart0.append("table")
-             .attr("class", "table")
-             .style("width", "300px");
-             thead = table.append("thead")
-         tbody = table.append("tbody");
+    looking_glass.init = function(chart0) {
+        var table = chart0.append("table")
+            .attr("class", "table")
+            .style("width", "300px");
+        thead = table.append("thead")
+        tbody = table.append("tbody");
 
-         thead.append("tr")
-             .selectAll("th")
-             .data(["key", "metric"])
-             .enter()
-             .append("th")
-             .text(function(column) { return column; });
-     };
+        thead.append("tr")
+            .selectAll("th")
+            .data(["key", "metric"])
+            .enter()
+            .append("th")
+            .text(function(column) { return column; });
+    };
 
-     looking_glass.render = function() {
-         var indexData = [];
-         for (var key in index) {
-             indexData.push(index[key]);
-         };
+    looking_glass.render = function() {
+        var indexData = [];
+        for (var key in index) {
+            indexData.push(index[key]);
+        };
 
-         var t = tbody.selectAll("div").data(indexData);
-         var rows = tbody.selectAll("tr")
-             .data(indexData)
-         rows.enter().append("tr");
+        var t = tbody.selectAll("div").data(indexData);
+        var rows = tbody.selectAll("tr")
+            .data(indexData)
+        rows.enter().append("tr");
 
-         var cells = rows.selectAll("td")
-             .data(function(row) {
-                 return ['key', 'value'].map(
-                     function(column) {
-                         return {column: column, value: row[column]};
-                     });
-             });
-         cells.enter().append("td");
-         var bars = cells.selectAll("div")
-             .data(function(d) { return [d]; })
-         bars.enter().append("div").call(render_div)
-         bars.transition().call(render_div)
+        var cells = rows.selectAll("td")
+            .data(function(row) {
+                return ['key', 'value'].map(
+                    function(column) {
+                        return {column: column, value: row[column]};
+                    });
+            });
+        cells.enter().append("td");
+        var bars = cells.selectAll("div")
+            .data(function(d) { return [d]; })
+        bars.enter().append("div").call(render_div)
+        bars.transition().call(render_div)
 
-         function render_div() {
-             this
-                 .text(function(d) { return d.value; })
-                 .filter(function(d) { return d.column == 'value'; })
-                 .attr("class", "ok")
-                 .style("width", function(d) { return d.value * 100 + "px"; });
-         };
+        function render_div() {
+            this
+                .text(function(d) { return d.value; })
+                .filter(function(d) { return d.column == 'value'; })
+                .attr("class", "ok")
+                .style("width", function(d) { return d.value * 100 + "px"; });
+        };
 
-         cells.exit().remove();
-         bars.exit().remove();
-     };
+        cells.exit().remove();
+        bars.exit().remove();
+    };
 
 
     var svg;
@@ -88,116 +88,110 @@ var path = sankey.link();
             .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
     };
 
-     looking_glass.render_flow = function () {
-         var graph = to_lists();
+    looking_glass.render_flow = function () {
+        var graph = to_lists();
 
-         sankey
-             .nodes(graph.nodes)
-             .links(graph.links)
-             .layout(32);
+        sankey
+            .nodes(graph.nodes)
+            .links(graph.links)
+            .layout(32);
 
-         // remove elements from previous run
-         svg.selectAll("path").remove();
-         svg.selectAll("link").remove();
-         svg.selectAll("rect").remove();
-         svg.selectAll("text").remove();
+        // remove elements from previous run
+        svg.selectAll("path").remove();
+        svg.selectAll("link").remove();
+        svg.selectAll("rect").remove();
+        svg.selectAll("text").remove();
 
-         var link = svg.append("g").selectAll(".link")
-             .data(graph.links)
-             .enter().append("path")
-             .attr("class", "link")
-             .attr("d", path)
-             .style("stroke-width", function(d) { return Math.max(1, d.dy); })
-             .sort(function(a, b) { return b.dy - a.dy; });
+        var link = svg.append("g").selectAll(".link")
+            .data(graph.links, function(d) { return d.key })
+        link.enter().append("path")
+            .attr("class", "link")
+            .attr("d", path)
+            .style("stroke-width", function(d) { return Math.max(1, d.dy); })
+            .sort(function(a, b) { return b.dy - a.dy; });
 
-         link.append("title")
-             .text(function(d) {
-                 return d.source.name + " → " + d.target.name + "\n" + format(d.value);
-             });
 
-         var node = svg.append("g").selectAll(".node")
-             .data(graph.nodes)
-             .enter().append("g")
-             .attr("class", "node")
-             .attr("transform", function(d) {
-                 return "translate(" + d.x + "," + d.y + ")";
-             });
+        var node = svg.append("g").selectAll(".node")
+            .data(graph.nodes, function(d) { return d.id })
+            .enter().append("g")
+            .attr("class", "node")
+            .attr("transform", function(d) {
+                return "translate(" + d.x + "," + d.y + ")";
+            });
+        node.append("rect")
+            .attr("height", function(d) { return d.dy; })
+            .attr("width", sankey.nodeWidth())
+            .style("fill", function(d) {
+                return d.color = color(d.name.replace(/ .*/, ""));
+            })
+            .style("stroke", function(d) { return d3.rgb(d.color).darker(2); })
+            .append("title")
+            .text(function(d) { return d.name + "\n" + format(d.value); });
+        node.append("text")
+            .attr("x", -6)
+            .attr("y", function(d) { return d.dy / 2; })
+            .attr("dy", ".35em")
+            .attr("text-anchor", "end")
+            .attr("transform", null)
+            .text(function(d) { return d.name; })
+            .filter(function(d) { return d.x < width / 2; })
+            .attr("x", 6 + sankey.nodeWidth())
+            .attr("text-anchor", "start");
+    };
 
-         node.append("rect")
-             .attr("height", function(d) { return d.dy; })
-             .attr("width", sankey.nodeWidth())
-             .style("fill", function(d) {
-                 return d.color = color(d.name.replace(/ .*/, ""));
-             })
-             .style("stroke", function(d) { return d3.rgb(d.color).darker(2); })
-             .append("title")
-             .text(function(d) { return d.name + "\n" + format(d.value); });
+    function subscribe_common(host, query, handler) {
+        console.log('subscribing (' + host + ' / ' + query + ') ...');
+        var queryString = "query=" + encodeURI(query);
+        var uri = "ws://" + host + "/index?subscribe=true&" + queryString;
+        // if user is running mozilla then use it's built-in WebSocket
+        window.WebSocket = window.WebSocket || window.MozWebSocket;
+        var connection = new WebSocket(uri);
+        connection.onopen = function () {
+            console.log('ws opened (' + host + ' / ' + query + ')');
+        };
+        connection.onerror = function (error) {
+            console.log('ws error (' + host + ' / ' + query + ')');
+        };
+        connection.onmessage = function (message) {
+            try {
+                var json = JSON.parse(message.data);
+            } catch (e) {
+                console.log('invalid JSON: ', message.data);
+                return;
+            };
+            handler(json);
+            var item = handler(json);
+        };
+    };
 
-         node.append("text")
-             .attr("x", -6)
-             .attr("y", function(d) { return d.dy / 2; })
-             .attr("dy", ".35em")
-             .attr("text-anchor", "end")
-             .attr("transform", null)
-             .text(function(d) { return d.name; })
-             .filter(function(d) { return d.x < width / 2; })
-             .attr("x", 6 + sankey.nodeWidth())
-             .attr("text-anchor", "start");
-     };
+    looking_glass.subscribe = function (host, query, handler) {
+        subscribe_common(host, query, function(json) {
+            var item = handler(json);
+            index[item.key] = item;
+        });
+    };
 
-     looking_glass.subscribe = function (host, query, handler) {
-         subscribe_common(host, query, function(json) {
-             var item = handler(json);
-             index[item.key] = item;
-             //redraw();
-         });
-     };
+    looking_glass.subscribe_flow = function (host, query, handler) {
+        subscribe_common(host, query, function(json) {
+            var item = handler(json);
+            if (! item) {
+                return;
+            };
 
-     looking_glass.subscribe_flow = function (host, query, handler) {
-         subscribe_common(host, query, function(json) {
-             var item = handler(json);
-             if (! item) {
-                 return;
-             };
+            ensure_id(item.source);
+            ensure_id(item.target);
+            var source_id = nodes[item.source].id;
+            var target_id = nodes[item.target].id;
 
-             ensure_id(item.source);
-             ensure_id(item.target);
-             var source_id = nodes[item.source].id;
-             var target_id = nodes[item.target].id;
-
-             var key = [source_id, target_id];
-             //var key = [item.source, item.target];
-             edges[key] = {"source": source_id,
-                           "target": target_id,
-                           "value": item.value};
-         });
-     };
-
-     function subscribe_common(host, query, handler) {
-         console.log('subscribing (' + host + ' / ' + query + ') ...');
-         var queryString = "query=" + encodeURI(query);
-         var uri = "ws://" + host + "/index?subscribe=true&" + queryString;
-         // if user is running mozilla then use it's built-in WebSocket
-         window.WebSocket = window.WebSocket || window.MozWebSocket;
-         var connection = new WebSocket(uri);
-         connection.onopen = function () {
-             console.log('ws opened (' + host + ' / ' + query + ')');
-         };
-         connection.onerror = function (error) {
-             console.log('ws error (' + host + ' / ' + query + ')');
-         };
-         connection.onmessage = function (message) {
-             try {
-                 var json = JSON.parse(message.data);
-             } catch (e) {
-                 console.log('invalid JSON: ', message.data);
-                 return;
-             };
-             handler(json);
-             var item = handler(json);
-         };
-     };
-
+            var key = [source_id, target_id];
+            //var key = [item.source, item.target];
+            edges[key] = {
+                "key": key,
+                "source": source_id,
+                "target": target_id,
+                "value": item.value};
+        });
+    };
 
     function to_lists() {
         var node_list = [];
@@ -211,13 +205,13 @@ var path = sankey.link();
         return {"nodes": node_list, "links": edge_list};
     };
 
-     function ensure_id(item_key) {
-         if (nodes[item_key] == undefined) {
-             var id = max_id;
-             max_id += 1;
-             nodes[item_key] = {"name": item_key, "id": id};
-         };
-     };
+    function ensure_id(item_key) {
+        if (nodes[item_key] == undefined) {
+            var id = max_id;
+            max_id += 1;
+            nodes[item_key] = {"name": item_key, "id": id};
+        };
+    };
 
-     console.log('looking_glass finished initializing');
- })();
+    console.log('looking_glass finished initializing');
+})();
